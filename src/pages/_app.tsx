e192dc16b8381f91b7652d5f5bfe07ae71c16fd3
Humberto Rivero <humbertorivero9@gmail.com>
2023-05-22 16:51:04 -0300

    Add MUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import {CssBaseline, ThemeProvider, createTheme} from "@mui/material";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const basicTheme = createTheme({
+  palette: {
+    mode: "dark",
+  }
+})
+
+export default function App({Component, pageProps}: AppProps) {
+	return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
